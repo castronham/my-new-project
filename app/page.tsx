e30caba1bf74d8a73495c20d22d4b536314675a8
Castro Ncham <castrohakeem@gmail.com>
2026-05-09 02:21:42 +0800

    Initial commit for Titan project

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main>
+      <p>I am a freelance Web Developer with 5 years of experience specializing in WordPress and modern web frameworks. I focus on building high performance, SEO optimized websites that help businesses grow their online presence and reach global audiences.</p>
+      </main>
+  );
+      }
